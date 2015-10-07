11ec8074b50d15a2e57a85e73d7e4c4f3c280b33
matthacker <matthacker.alex@gmail.com>
2015-10-07 21:41:57 +0200

    Made a ComboBox instead of textblock, added a new class with som specialities.

---
+++ Delopgave3/Speciality.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delopgave3
+{
+    class Speciality : ObservableCollection<string>
+    {
+        public  Speciality()
+        {
+            Add("Assassination");
+            Add("CodeGenius");
+            Add("BabyMaker");
+            Add("IceCreamMaker");
+            Add("Systematicer");
+        }
+    }
+}
