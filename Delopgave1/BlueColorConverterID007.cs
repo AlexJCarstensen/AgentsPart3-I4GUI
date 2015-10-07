31314d6dbeb25b40d1cfa1a3290565b7b056f478
matthacker <matthacker.alex@gmail.com>
2015-10-07 17:56:28 +0200

    new

---
+++ Delopgave1/BlueColorConverterID007.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Delopgave14
+{
+    class BlueColorConverterID007 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var id = value as string ?? "";
+
+            return (id == "007" ? Brushes.Blue : Brushes.Black); 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
